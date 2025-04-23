7c0fb10ce36a33be63e3297921a2771233506507
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-04-23 15:39:40 +0100

    d

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,9 +1,13 @@
 import { Inter, Roboto } from "next/font/google";
 import { Work_Sans } from "next/font/google";
 import { Lato } from "next/font/google";
-const inter = Inter();
+
 
 //login page
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
 
 export const sidebarItems = Work_Sans({
   weight: ["400", "700", "900"],
@@ -13,10 +17,11 @@ export const sidebarItems = Work_Sans({
 
 export const Doc = Roboto({
   weight: ["400", "500", "600"],
+  subsets: ["latin"], // 👈 Add this line
 });
 
 export const tabsFont = Lato({
-weight: ["400"],
+  weight: ["400"],
   fallback: ["Roboto", "Arial", "sans-serif"],
   subsets: ["latin"],
 });
