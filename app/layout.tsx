c89fc36824813bc5fa806ac12683e37b1f7378fc
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-04-24 10:34:49 +0100

    feat: add favicon.ico and update layout component to include favicon link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,26 @@
-import React from "react"
-import "../styles/globals.scss"
-import type { Metadata } from "next"
-import { ReactQueryProvider } from "../providers/reactQueryProvider"
+import React from "react";
+import "../styles/globals.scss";
+import type { Metadata } from "next";
+import { ReactQueryProvider } from "../providers/reactQueryProvider";
 
 export const metadata: Metadata = {
   title: "Lendsqr - Login",
   description: "Lendsqr login page",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <ReactQueryProvider>
-
-        <body>{children}</body> 
+        <body>{children}</body>
       </ReactQueryProvider>
     </html>
-  )
+  );
 }
